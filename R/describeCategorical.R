6778e916804d9098475140d2806f277391bcfa03
ofurkusi <palmar.thorsteinsson@gmail.com>
2016-01-06 23:26:07 +0000

    Allow the user to switch on or off display of cumulative and valid only columns and NA values when describing categorical variables.

--- R/describeCategorical.R
+++ R/describeCategorical.R
@@ -1,4 +1,5 @@
-describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label="") {
+describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label="",
+                                cumulative=TRUE, validonly=FALSE, showNA=TRUE) {
 
   if (is.data.frame(data)) {
     data <- data[,1]
@@ -8,16 +9,15 @@ describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label=
   if (!is.factor(data)) {
     stop("Categorical variables must be defined as factors")
   }
-  
-  # Text labels
-  txtValid   <- gettext("Valid answers", domain="R-limestats")
-  txtInvalid <- gettext("Invalid answers", domain="R-limestats")
-  txtAll     <- gettext("All answers", domain="R-limestats")
-  txtSum     <- gettext("Total", domain="R-limestats")
-  txtNA      <- gettext("No answer", domain="R-limestats")
-  txtOption  <- gettext("Option", domain="R-limestats")
 
-  showNA     <- TRUE
+  # Text labels
+  txtValid   <- gettext("Valid answers",   domain="R-limestats")
+  txtInvalid <- gettext("Invalid answers", domain="R-limestats")
+  txtAll     <- gettext("All answers",     domain="R-limestats")
+  txtSum     <- gettext("Total",           domain="R-limestats")
+  txtNA      <- gettext("No answer",       domain="R-limestats")
+  txtOption  <- gettext("Option",          domain="R-limestats")
+
   emptyCell  <- NA
 
   # Get the number of different valid options
@@ -26,18 +26,18 @@ describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label=
   propTable <- as.vector(prop.table(table(data))*100)
 
   #Check if there are any #NA's
-  if (!any(is.na(data))) {
-    txtInvalid <- NULL
-    txtNA <- NULL
-    showNA <- FALSE
-    emptyCell  <- NULL
-  }
+#  if (!any(is.na(data))) {
+#    txtInvalid <- NULL
+#    txtNA <- NULL
+#    showNA <- FALSE
+#    emptyCell  <- NULL
+#  }
   
   stats <- data.frame(
     Type = c(txtValid, rep("", times=levelCount-1), txtInvalid),
     Labels = c(levels(data), txtNA),
-    Frequency = as.vector(table(data, useNA="ifany")),
-    PercentTotal = as.vector(as.vector(prop.table(table(data, useNA="ifany")))*100)
+    Frequency = as.vector(table(data, useNA="always")),
+    PercentTotal = as.vector(as.vector(prop.table(table(data, useNA="always")))*100)
     #Percent = as.vector(format(prop.table(table(data))*100, digits=1, decimal.mark="."))
   )
   
@@ -65,7 +65,7 @@ describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label=
   #Rearrange the rows
   if (showNA) {
     stats <- rbind(stats[1:levelCount,], stats[nrow(stats),], stats[nrow(stats)-1,])
-  } else {
+  #} else {
     #stats <- rbind(stats[1:levelCount,], stats[nrow(stats),], stats[nrow(stats)-1,])
   }
   
@@ -78,28 +78,55 @@ describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label=
   stats$PercentValid <- as.numeric(stats$PercentValid)
   stats$CumulativePercent <- as.numeric(stats$CumulativePercent)
   
+
   if (latex==TRUE) {
-    colNames <- colnames(stats)
-    colnames(stats) <- c("",
-                         paste("\\textrm{", txtOption, "}", sep=""),
-                         "$\\mathrm{n}$",
-                         "$\\mathrm{\\%}$",
-                         "$\\mathrm{\\%}$",
-                         "$\\mathrm{\\sum \\%}$")
+    # NOTE: lenght of align and digits is 1+ncol if table is dataframe, however that does not apply to head
+    columnParts <- data.frame(
+      head = c(NA,
+               "",
+               paste("\\textrm{", txtOption, "}", sep=""),
+               "$\\mathrm{n}$",
+               "$\\mathrm{\\%}$",
+               "$\\mathrm{\\%}$",
+               "$\\mathrm{\\sum \\%}$"),
+      align = c("l", "l", "l", "r", "r", "r", "r"),
+      digits = c(0, 0, 0,0, digits, digits, digits),
+      midrule = c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE),
+      seq = c(NA, seq(from=1, to=6))
+    )
+    rownames(columnParts) <- c("rowname", "Type", "Labels", "Frequency", "PercentTotal", "PercentValid", "CumulativePercent")
   
+    # Remove rows that are not to be displayed
+    rowsToShow    <- c(seq(from=1, to=levelCount), (levelCount+1)*showNA, levelCount+2, (levelCount+3)*showNA)
+    stats <- stats[rowsToShow,]
+    
+    # Remove column parts that are not being used
+    columnsToShow <- c(TRUE, showNA, TRUE, TRUE, showNA, TRUE, cumulative)
+    columnParts <- columnParts[columnsToShow, ]
+    #columnParts <- columnParts[setdiff(rownames(columnParts),columnsToHide),]
+    
+    # Remove columns from the "stats" table that are not to be displayed
+    stats <- stats[columnParts$seq[!is.na(columnParts$seq)]]
+    
+    #colNames <- colnames(stats)
+    colnames(stats) <- columnParts$head[!is.na(columnParts$head)]
+    
     midRule  <- "\\midrule \n"
-    cmidRule <- "\\cmidrule{2-6} \n"
-    
-    tableDigits <- c(0, 0, 0,0, digits, digits, digits) # define number of digits for each column
-    
-    latexTable <- xtable(stats, caption=caption, label=label, digits=tableDigits)
-    align(latexTable) <- c("l", "l", "l", "r", "r", "r", "r")
-    
-    if (showNA) {
-      posList <- list(nrow(stats)-3,nrow(stats)-2)      
+    if(showNA) {
+      cmidRule <- sprintf("\\cmidrule{%d-%d} \n", table(columnParts$midrule)[1], table(columnParts$midrule)[1]+table(columnParts$midrule)[2]-1)
     } else {
-      posList <- list(nrow(stats)-2,nrow(stats)-1)
+      cmidRule <- NULL
     }
+    
+    latexTable <- xtable(stats, digits=columnParts$digits, caption=caption, label=label)
+    align(latexTable) <- as.character(columnParts$align)
+
+    if (showNA) {
+      posList <- list(nrow(stats)-3,nrow(stats)-2)
+    } else {
+      posList <- list(nrow(stats)-1)
+    }
+    
     
     print(latexTable, booktabs = TRUE, floating = TRUE, type = "latex", table.placement="H",
           add.to.row=list(pos=posList, command=c(cmidRule,midRule)),
@@ -108,7 +135,7 @@ describeCategorical <- function(data, digits = 1, latex=TRUE, caption="", label=
           scalebox=0.9, include.rownames=FALSE,
           sanitize.text.function = function(x){x}) #hline.after = hlines, 
   
-    colnames(stats) <- colNames
+    #colnames(stats) <- colNames
   } else {
     return(stats)
   }
